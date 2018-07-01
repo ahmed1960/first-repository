ff8dbb787ccebff0dad17e07a4715991d62e568f
ahmed1960 <40655388+ahmed1960@users.noreply.github.com>
2018-07-01 09:57:06 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -14,6 +14,7 @@ public class Calculator extends Application {
 	/*
 	 * this is simple calculator class    
 	 *yanal zayed
+	 *ahmed zayed 
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
